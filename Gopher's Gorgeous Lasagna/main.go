cacc15ac127fded5986f90ea029bca722e20d223
Filipe Coelho <coelho.sox@gmail.com>
2022-03-17 22:53:51 +0000

    comments added

--- Gopher's Gorgeous Lasagna/main.go
+++ Gopher's Gorgeous Lasagna/main.go
@@ -1,16 +1,25 @@
+// In this exercise I'm going to write some code 
+//to help me cook a brilliant lasagna from my favorite cooking book.
+
+
+
 package main
 
+//  Expected oven time in minutes
 const OvenTime = 40
 
+// Calculation of the remaining oven time in minutes
 func RemainingOvenTime(actual int) int {
 	return OvenTime - actual
 }
 
+// Calculation of the preparation time in minutes
 func PreparationTime(numberOfLayers int) int {
 	preparation := 2
 	return preparation * numberOfLayers
 }
 
+// Calculation of the elapsed working time in minutes
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
 	return PreparationTime(numberOfLayers) + actualMinutesInOven
 }
